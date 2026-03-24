6f7c3d1a0b121f1f7b7f055618ff675d993f0557
mindmonkey2 <shushruthgadipelli@gmail.com>
2026-03-24 18:06:53 +0530

    chore: add firebase_ai ^3.10.0, fix pubspec.lock corruption

--- spring_health_member_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ spring_health_member_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,8 @@ import Foundation
 import cloud_firestore
 import device_info_plus
 import file_selector_macos
+import firebase_ai
+import firebase_app_check
 import firebase_auth
 import firebase_core
 import firebase_messaging
@@ -21,6 +23,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FirebaseAIPlugin.register(with: registry.registrar(forPlugin: "FirebaseAIPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
